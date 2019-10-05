dc01b926c8a07d64ba258c92a0dc22ff8460fc3a
Ilya <44731656+mrmiv@users.noreply.github.com>
2019-10-05 12:44:19 +0700

    Настройка Django 1.1.2 - Добавлены страницы

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secret.py import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,9 +23,6 @@ STATICFILES_DIRS = [
 ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*#y#re(&cbmejrn=7z^kkiyzbdse55^%9lj#62lcw62np1w@h9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
