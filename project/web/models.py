250819b246021ed20ed2579936902b988057bdf9
Ilya <44731656+mrmiv@users.noreply.github.com>
2019-10-06 18:41:02 +0700

    Настройка модели еды v0.1

--- project/web/models.py
+++ project/web/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 # Модель еды (порции) с ее атрибутами
-class food(models.Model):
+class food_model(models.Model):
     # название блюда
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25, unique=True)
     # общее количество порций (в шт/в граммах)
     all_count = models.IntegerField()
     # количество блюда. заказано порций в штуках
