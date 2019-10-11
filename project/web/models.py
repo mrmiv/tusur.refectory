209485238f8fa0d0b8445eb5b3f1abe842df674e
Ilya <44731656+mrmiv@users.noreply.github.com>
2019-10-11 10:39:22 +0700

    Настройка функционала получения модели из меню в корзину v0.1

--- project/web/models.py
+++ project/web/models.py
@@ -3,18 +3,26 @@ from django.db import models
 # Create your models here.
 
 # Модель еды (порции) с ее атрибутами
-class food_model(models.Model):
-    # название блюда
-    name = models.CharField(max_length=25, unique=True)
-    # общее количество порций (в шт/в граммах)
-    all_count = models.IntegerField()
-    # количество блюда. заказано порций в штуках
-    count = models.IntegerField()
-    # время заказа
-    time = models.DateTimeField()
-    # цена порции (dp - разряд рублей, md - копейки )
-    price = models.DecimalField(decimal_places=3, max_digits=5)
-    # изображение блюда
-    # image = models.ImageField()
+# class food_model(models.Model):
+#     # название блюда
+#     name = models.CharField(max_length=25, unique=True)
+#     # общее количество порций (в шт/в граммах)
+#     all_count = models.IntegerField()
+#     # количество блюда. заказано порций в штуках
+#     count = models.IntegerField(default=0)
+#     # время заказа
+#     # time = models.DateTimeField()
+#     # цена порции (dp - разряд рублей, md - копейки )
+#     price = models.DecimalField(decimal_places=3, max_digits=5)
+#     # изображение блюда
+#     # image = models.ImageField()
+
+class food(models.Model):
+    name = models.CharField(max_length=25)
+    count = models.IntegerField(default=0)
+    price = models.DecimalField(decimal_places=3, max_digits=5, default=3)
+
+    # Настройка функционала получения модели из меню в корзину v0.1
+
 
 
---
+++ project/web/static/js/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var form = $('#form-buy')
+    // console.log(form);
+    form.on('submit', function(e){
+        e.preventDefault()
+        // console.log('123');
+        var number = $("#number").val();
+        // console.log(number);
+        // var product_id =
+        // var product_name = 
+    })
+});
