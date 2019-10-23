8d7e5ac7d195b66c8f490d149027427c205e342b
Ilya <44731656+mrmiv@users.noreply.github.com>
2019-10-24 02:03:12 +0700

    create models for auth

--- project/web/models.py
+++ project/web/models.py
@@ -17,13 +17,48 @@ from django.db import models
 #     # изображение блюда
 #     # image = models.ImageField()
 
-class food(models.Model):
-    name = models.CharField(max_length=25)
-    price = models.DecimalField(decimal_places=2, max_digits=5, default=3)
+# class food(models.Model):
+#     name = models.CharField(max_length=25)
+#     price = models.DecimalField(decimal_places=2, max_digits=5, default=3)
 
-class shop(models.Model):
-    name = models.CharField(max_length=25)
-    price = models.DecimalField(decimal_places=2, max_digits=5, default=3)
-    count = models.IntegerField(default=1)
+# class shop(models.Model):
+#     name = models.CharField(max_length=25)
+#     price = models.DecimalField(decimal_places=2, max_digits=5, default=3)
+#     count = models.IntegerField(default=1)
+
+
+from django.db import models
+
+# Блюдо
+class product(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    category = models.CharField(max_length=25)
+    #количество
+    count = models.IntegerField(default=0)
+      #состав
+    structure = models.CharField(max_length=150)
+
+# Пользователь
+class user(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    
+# Заказы
+class orders(models.Model):
+    dateorder = models.DateField()
+    timeorder = models.TimeField()
+    pay = models.BooleanField()
+    totalprice = models.DecimalField(decimal_places=2, max_digits=5)
+    # статус выдачи
+    deliverystatus = models.BooleanField()
+    products = models.ManyToManyField(product)
+    # Корпус выдачи:
+    # building = CharField(max_length=5)
+
+# Сотрудники
+# username = EmailField()
+# password = TextField()
+
 
 
