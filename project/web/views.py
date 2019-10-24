c751d1bb80ffc75cbe30fc6d269ab43171d5b822
kristina-titaeva <56391761+kristina-titaeva@users.noreply.github.com>
2019-10-24 12:08:03 +0700

    create user v0

--- project/web/views.py
+++ project/web/views.py
@@ -1,18 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django import forms
-# from .models import food, shop
+# from .models import Userform, user
+from django.contrib.auth import authenticate, user
 import json
 
 def index(request):
-    eat = food.objects.all()
-    return(render(request, 'main.html',{"food": eat}))
+    userform = Userform()
+    return(render(request, 'main.html', {"form": Userform}))
 
 def index_menu(request):
     return(render(request, 'menu.html'))
 
 def index_shop(request):
-    return(render(request, 'shop.html'))
+    return(render(request, 'shop.html')) 
+
+# def auth(request):
+    # if request.method == 'POST':
+    #     user_form = Userform(request.POST, instance=request.user)
+    #     if user.is_anonymous():
+    #         if user_form.is_valid():
+    #             user_form.save()
+    #             user.authenticate(name=)
+    #             return HttpResponse("<h2>Hello, {0}</h2>".format(name, email))
+    #         else:
+    #             return 
+
+def auth(request):
+    if request.method == 'POST':
+        user_form = Userform(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+
+        # profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        
+            # messages.error(request, _('Пожалуйста, исправьте ошибки.'))
+    else:
+        user_form = Userform(instance=request.user)
+        # profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'main.html', {
+        'user_form': user_form,
+        # 'profile_form': profile_form
+    })
+
 
 # Create your views here.
 
