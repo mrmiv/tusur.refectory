231b7e457ce9f33128fff63fe151ef10e25173a9
Ilya <44731656+mrmiv@users.noreply.github.com>
2019-10-31 23:47:08 +0700

    little changes

--- refectory/general/static/js/animate.js
+++ refectory/general/static/js/animate.js
@@ -1,25 +1,22 @@
 $(document).ready(function(){
     parallaxScroll();
-    
-    console.log($('.block-info').offset().top-$(window).height()*0.9)
-    // console.log(
-    //     $('.block-text-info').offset().top-$(window).height()
-    // )
     $(window).bind('scroll',function(){
         parallaxScroll();
     });
 })
 
 function parallaxScroll(){
-    var scrolled = $(window).scrollTop();
-    console.log(scrolled)
+    // var scrolled = $(window).scrollTop();
+    // console.log(scrolled)
     // Нижнее поле
-    if (scrolled>=$('.block-info').offset().top-$(window).height()*0.9) {
-        $('.block-text-info').addClass('animated flipInX');
+    if (scrolled>=$('.block-info').offset().top-$(window).height()) {
+        $('.block-text-info').addClass('flipInX');
     }
     else{
-        // $('.block-text-info').css('opacity', '0');
+
     }
+
+
     //Поле по середине
     if (scrolled>=$('.block-about').offset().top-$(window).height()*0.9) {
         $('.text-about').addClass('animated fadeIn');
@@ -27,12 +24,14 @@ function parallaxScroll(){
     else{
         $('.text-about').css('opacity', '0');
     }
+
+
     //Вторая картинка
-    if (scrolled>=$('.block-img.second-img').offset().top-$(window).height()*0.9) {
-        $('.block-img.second-img').addClass('animated flipInX');
+    if (scrolled>=$('.second-img').offset().top-$(window).height()*0.9) {
+        $('.second-img').addClass('animated flipInX');
     }
     else{
-        //$('.block-img.second-img').css('opacity', '0');
+        $('.second-img').css('opacity', '0');
     }
     
 };
